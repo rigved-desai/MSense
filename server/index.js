72b5568a5dfc79a15f4d6338cf2c996268c63a99
Rigved Desai <rigveddesai@gmail.com>
2023-05-15 03:37:22 +0530

    OK

--- server/index.js
+++ server/index.js
@@ -4,16 +4,17 @@ const {PORT} = require('../server/config/config')
 const cors = require('cors');
 require('dotenv').config()
 const { spawn } = require('child_process');
+const fileUploadRouter = require("./routes/fileUploadRouter");
+app.use(cors());
+app.use("/", fileUploadRouter);
 
 const lol = async() => {
     await spawn('python', ['modelapi/modelAPI.py']);
+    await app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 }
 
 lol();
 
-const fileUploadRouter = require("./routes/fileUploadRouter");
 
-app.use(cors());
-app.use("/", fileUploadRouter);
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+
