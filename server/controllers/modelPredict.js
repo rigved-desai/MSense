5c7b4e0fa081522ef10ae95a6ddb7b6729902466
Rigved Desai <rigveddesai@gmail.com>
2023-05-15 01:58:27 +0530

    Updated requirements.txt

--- server/controllers/modelPredict.js
+++ server/controllers/modelPredict.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const modelPort = process.env.MODELPORT || 5000;
 
 exports.predictResult = async(req, res, next) =>  {
-    axios.get(`http://localhost:${modelPort}/predict`) // change in prod
+    axios.get(`http://localhost:${modelPort}/predict`) 
         .then((response) => {
             let prediction = response.data.prediction;
             if(prediction == 1) return res.status(200).json({result: true, message: "File uploaded and processed successfully!", malware: false});
