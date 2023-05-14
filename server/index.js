e4eeee317f4c34c527c4a34bc6ca1b18656a8ecb
Rigved Desai <rigveddesai@gmail.com>
2023-05-15 02:27:15 +0530

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -5,10 +5,9 @@ const cors = require('cors');
 require('dotenv').config()
 const { spawn } = require('child_process');
 
-const modelAPI = spawn('python', ['modelAPI/modelAPI.py']);
+const modelAPI = spawn('python', ['modelapi/modelAPI.py']);
 
 const fileUploadRouter = require("./routes/fileUploadRouter");
-const { model } = require('@tensorflow/tfjs');
 
 app.use(cors());
 app.use("/", fileUploadRouter);
